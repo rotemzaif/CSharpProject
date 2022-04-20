ddd62084487c3a527d0a7ed5fa9914010e334189
Rotem Zaif <rotemz@supercom.com>
2022-04-20 14:19:47 +0300

    uncommented several tests

--- Tests/LoginTest.cs
+++ Tests/LoginTest.cs
@@ -41,40 +41,40 @@ namespace FrameworkHW2_SwagLabs.Tests
 
         }
 
-        //[Test, Description("ordered logout from the app - via logout button")]
-        //[AllureSeverity(SeverityLevel.normal)]
-        //[AllureFeature("logout")]
-        //public void TC02_Logout()
-        //{
-        //    pp = new ProductsPage(driver);
-        //    pp.OpenOptionsMenu();
-        //    pp.SelectAction(MenuOption.LOGOUT);            
-        //    lp = new LoginPage(driver);
-        //    Assert.That(lp.IsPageDisplayed(lp.LoginBtn));
-        //}
+        [Test, Description("ordered logout from the app - via logout button")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureFeature("logout")]
+        public void TC02_Logout()
+        {
+            pp = new ProductsPage(driver);
+            pp.OpenOptionsMenu();
+            pp.SelectAction(MenuOption.LOGOUT);
+            lp = new LoginPage(driver);
+            Assert.That(lp.IsPageDisplayed(lp.LoginBtn));
+        }
 
-        //[Test, Description("app login with invalid credentials and get an error message")]
-        //[AllureSeverity(SeverityLevel.critical)]
-        //[AllureFeature("login")]
-        //public void TC03_Login_With_Invalid_Credentials()
-        //{
-        //    LoginPage lp = new LoginPage(driver);
-        //    lp.Login("rotem", "123456789");
-        //    Assert.That(lp.IsErrMsgDisplayed(), "Error message is not displayed when entering invalid credentials");
-        //    //string expectedErrMsg = Utils.pageElements["loginPage:labels_text:invalid_credentials_err_msg"];
-        //    //Console.WriteLine("actual err msg: " + lp.GetErrMsgText());
-        //    //Assert.That(lp.GetErrMsgText(), Is.EqualTo(expectedErrMsg));
-        //}
+        [Test, Description("app login with invalid credentials and get an error message")]
+        [AllureSeverity(SeverityLevel.critical)]
+        [AllureFeature("login")]
+        public void TC03_Login_With_Invalid_Credentials()
+        {
+            LoginPage lp = new LoginPage(driver);
+            lp.Login("rotem", "123456789");
+            Assert.That(lp.IsErrMsgDisplayed(), "Error message is not displayed when entering invalid credentials");
+            //string expectedErrMsg = Utils.pageElements["loginPage:labels_text:invalid_credentials_err_msg"];
+            //Console.WriteLine("actual err msg: " + lp.GetErrMsgText());
+            //Assert.That(lp.GetErrMsgText(), Is.EqualTo(expectedErrMsg));
+        }
 
-        //[Test, Description("check error message content")]
-        //[AllureSeverity(SeverityLevel.minor)]
-        //[AllureFeature("login")]
-        //public void TC04_check_error_message()
-        //{
-        //    LoginPage lp = new LoginPage(driver);
-        //    string expectedErrMsg = Utils.pageElements["loginPage:labels_text:invalid_credentials_err_msg"];
-        //    Assert.That(lp.GetErrMsgText(), Is.EqualTo(expectedErrMsg));
-        //}
+        [Test, Description("check error message content")]
+        [AllureSeverity(SeverityLevel.minor)]
+        [AllureFeature("login")]
+        public void TC04_check_error_message()
+        {
+            LoginPage lp = new LoginPage(driver);
+            string expectedErrMsg = Utils.pageElements["loginPage:labels_text:invalid_credentials_err_msg"];
+            Assert.That(lp.GetErrMsgText(), Is.EqualTo(expectedErrMsg));
+        }
 
 
     }
